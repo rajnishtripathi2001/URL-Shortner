773d747a20c6b672125faa72a7ce55c3038d62e9
Shivam Gupta <72003102+shivamgupta88@users.noreply.github.com>
2023-07-21 15:25:11 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { customAlphabet } = require("nanoid");
+const fs = require("fs");
+
+const app = express();
+const port = 3000;
+const urlDataFile = "urlData.json";
+
+// Function to read data from the JSON file
+function readUrlData() {
+  try {
+    const data = fs.readFileSync(urlDataFile, "utf8");
+    return JSON.parse(data);
+  } catch (err) {
+    return {};
+  }
+}
+
+// Function to write data to the JSON file
+function writeUrlData(data) {
+  fs.writeFileSync(urlDataFile, JSON.stringify(data, null, 2), "utf8");
+}
+
+// In-memory data store to store the shortened URLs.
+let urlStore = readUrlData();
+
+// Function to generate a random short URL using nanoid.
+const generateShortUrl = customAlphabet(
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+  6
+);
+
+app.use(express.json());
+
+
+app.post("/api/shorten", (req, res) => {
+    const { destination_url } = req.body;
+  
+    if (!destination_url) {
+      return res.status(400).json({ error: "Destination URL is required." });
+    }
+  
+    const short_url = generateShortUrl();
+    urlStore[short_url] = destination_url;
+  
+    writeUrlData(urlStore); // Update the file with the new data
+  
+    res.status(201).json({ short_url, destination_url });
+  });
+
+// Route to handle redirection from short URL to the original URL.
+app.get("/:short_url", (req, res) => {
+  const { short_url } = req.params;
+  const destination_url = urlStore[short_url];
+
+  if (!destination_url) {
+    return res.status(404).json({ error: "Short URL not found." });
+  }
+
+  res.redirect(destination_url);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
